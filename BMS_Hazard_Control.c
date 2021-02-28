2c09d6b4c7c881c4476e00db19be1f981c43d0dc
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-03-01 04:14:48 +0530

    Update BMS_Hazard_Control.c
    For any sort of system hazard all the system specific necessary functions can be added here.

--- BMS_Hazard_Control.c
+++ BMS_Hazard_Control.c
@@ -40,22 +40,22 @@ void Fc_System_shutdown(void)
 {
 	/***Shutdown Code**/
 	/*check the data transmission is done**/
-}
+}/*end of Fc_System_shutdown*/
 
 void Fc_Disconnect_Charger(void)
 {
 	/**Disconnect charger code**/
 	/*check the data transmission is done**/
-}
+}/*end of Fc_Disconnect_Charger*/
 
 void Fc_System_Sleep(void)
 {
 	/**System Sleep code**/
 	/*check the data transmission is done**/
-}
+}/*end of Fc_System_Sleep*/
 
 void Fc_System_Standby(void)
 {
 	/**System Standby Code**/
 	/*check the data transmission is done**/
-}
+}/*end of Fc_System_Standby*/
