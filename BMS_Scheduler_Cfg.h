49ed83b136828799ab0424d402c1b78cb4c69522
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-03-01 04:22:01 +0530

    Update BMS_Scheduler_Cfg.h
    This is a configuration file. If system is upgrading then developer can add any function in the arrays and the scheduler will call it.

--- BMS_Scheduler_Cfg.h
+++ BMS_Scheduler_Cfg.h
@@ -14,12 +14,12 @@
 
 /***********Configure the function needs tobe added here******************/
 
-CyclicTask_funcptr[] = {Fc_Temperature_Sensor_check,Fc_SOC_Sensor_check,Fc_Charge_Rate_Sensor_check,Fc_Hazard_Control};
-BckTask_funcptr[] = {Fc_Display_Msg,Fc_Battery_Check};
+CyclicTask_funcptrarray Cyc_aptr[] = {Fc_Temperature_Sensor_check,Fc_SOC_Sensor_check,Fc_Charge_Rate_Sensor_check,Fc_Hazard_Control};
+BckTask_funcptrarray Bg_aptr[] = {Fc_Display_Msg,Fc_Battery_Check};
 
 /************************************************************************/
 
-#define MAX_FG_TASK (sizeof(CyclicTask_funcptr)/sizeof(CyclicTask_funcptr[0]))
-#define MAX_BG_TASK (sizeof(BckTask_funcptr)/sizeof(BckTask_funcptr[0]))
+#define MAX_FG_TASK (sizeof(Cyc_aptr)/sizeof(Cyc_aptr[0]))
+#define MAX_BG_TASK (sizeof(Bg_aptr)/sizeof(Bg_aptr[0]))
 
 #endif /* BMS_SCHEDULER_CFG_H_ */
