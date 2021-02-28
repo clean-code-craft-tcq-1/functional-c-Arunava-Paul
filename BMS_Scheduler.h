61d4df7f505cdff9cc6370399b7b438ffd6efc37
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-03-01 04:20:05 +0530

    Update BMS_Scheduler.h
    changed the function pointer array names

--- BMS_Scheduler.h
+++ BMS_Scheduler.h
@@ -8,12 +8,13 @@
 #ifndef BMS_CYCLIC_TASK_H_
 #define BMS_CYCLIC_TASK_H_
 
-void Fc_Cyclic_task(void);
+extern void Fc_Cyclic_task(void);
 
 
 #define NULL_PTR  (void*)0
-typedef void (*CyclicTask_funcptr)(void);
-typedef void (*BckTask_funcptr)(void);
+typedef void (*CyclicTask_funcptrarray)(void);
+typedef void (*BckTask_funcptrarray)(void);
 
 
 #endif /* BMS_CYCLIC_TASK_H_ */
+
