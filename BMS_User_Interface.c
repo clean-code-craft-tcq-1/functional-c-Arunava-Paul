46339d004e46c05fe9bc4df113c7a5393145324b
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-03-02 02:29:07 +0530

    Update BMS_User_Interface.c
    Reducing CYC

--- BMS_User_Interface.c
+++ BMS_User_Interface.c
@@ -4,53 +4,24 @@
  *  Created on: Feb 27, 2021
  *      Author: PUL1KOR
  */
+#include "BMS_System_Check.h"
 #include "BMS_Common_Include.h"
 #include "BMS_User_Interface.h"
-#include "BMS_System_Check.h"
+#include "BMS_User_Interface_Cfg.h"
 
 extern Fault_type_en Fault_type_global;
 int Data_transmission_global = FALSE;
+const struct user_interface_st usr_interface[Fault_max_condition] = USER_INTERFACE_ACCUMULATED_DATA;
 
 void Fc_Display_Msg(void)
-{
-	switch(Fault_type_global)
+{	
+	int idx;
+	for(idx = 0 ; idx < Fault_max_condition ; idx++)
 	{
-	case Fault_none:
-		printf("\n System Ok \n");
-		break;
-	case Fault_low_temp:
-		printf("\n Warning!! System temperature low \n");
-			Data_transmission_global = TRUE;
-		break;
-	case Fault_high_temp:
-		printf("\n Warning!! System temperature high, Putting system into standby \n");
-			Data_transmission_global = TRUE;
-		break;
-	case Fault_soc_low:
-		printf("\n Warning!! System SOC low ,  Putting system into sleep , Please connect the vehicle to charge \n");
-			Data_transmission_global = TRUE;
-		break;
-	case Fault_soc_high:
-		printf("\n Warning!! System SOC is high, disconnecting from charger \n");
-			Data_transmission_global = TRUE;
-		break;
-	case Fault_chargeRate_low:
-		printf("\n Warning!! System charge rate is low \n");
-			Data_transmission_global = TRUE;
-		break;
-	case Fault_chargeRate_high:
-		printf("\n Warning!! System charge rate is high , disconnecting from charger \n");
-			Data_transmission_global = TRUE;
-		break;
-	case Fault_sensor_failure:
-		printf("\n Warning!! Sensor failure, Shutting down system \n");
-			Data_transmission_global = TRUE;
-		break;
-	default:
-		printf("\n Error!! Abnormal system condition. Putting system into sleep \n");
-			Data_transmission_global = TRUE;
-		break;
+		if(usr_interface[idx].sys_failure_identifier == Fault_type_global)
+		{
+			printf("\n%s\n", usr_interface[idx].user_msg_string);
+		}		
 	}
-	
+	Data_transmission_global = TRUE;
 }/*end of Fc_Display_Msg*/
-
