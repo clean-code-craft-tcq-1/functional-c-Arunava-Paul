343b285049ea33717d888417cad7ffc85c0b0f02
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-03-01 12:36:06 +0530

    Update BMS_User_Interface.c

--- BMS_User_Interface.c
+++ BMS_User_Interface.c
@@ -9,7 +9,7 @@
 #include "BMS_System_Check.h"
 
 extern Fault_type_en Fault_type_global;
-int Data_transmission_global;
+int Data_transmission_global = FALSE;
 
 void Fc_Display_Msg(void)
 {
@@ -20,29 +20,37 @@ void Fc_Display_Msg(void)
 		break;
 	case Fault_low_temp:
 		printf("\n Warning!! System temperature low \n");
+			Data_transmission_global = TRUE;
 		break;
 	case Fault_high_temp:
 		printf("\n Warning!! System temperature high, Putting system into standby \n");
+			Data_transmission_global = TRUE;
 		break;
 	case Fault_soc_low:
 		printf("\n Warning!! System SOC low ,  Putting system into sleep , Please connect the vehicle to charge \n");
+			Data_transmission_global = TRUE;
 		break;
 	case Fault_soc_high:
 		printf("\n Warning!! System SOC is high, disconnecting from charger \n");
+			Data_transmission_global = TRUE;
 		break;
 	case Fault_chargeRate_low:
 		printf("\n Warning!! System charge rate is low \n");
+			Data_transmission_global = TRUE;
 		break;
 	case Fault_chargeRate_high:
 		printf("\n Warning!! System charge rate is high , disconnecting from charger \n");
+			Data_transmission_global = TRUE;
 		break;
 	case Fault_sensor_failure:
 		printf("\n Warning!! Sensor failure, Shutting down system \n");
+			Data_transmission_global = TRUE;
 		break;
 	default:
 		printf("\n Error!! Abnormal system condition. Putting system into sleep \n");
+			Data_transmission_global = TRUE;
 		break;
 	}
-	Data_transmission_global = TRUE;
+	
 }/*end of Fc_Display_Msg*/
 
