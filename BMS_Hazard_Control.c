0a8f13ce88a54d208852f0c69c152ceabbe168cd
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-03-01 12:34:51 +0530

    Update BMS_Hazard_Control.c
    updating program stop code

--- BMS_Hazard_Control.c
+++ BMS_Hazard_Control.c
@@ -40,22 +40,30 @@ void Fc_System_shutdown(void)
 {
 	/***Shutdown Code**/
 	/*check the data transmission is done**/
+	if(Data_transmission_global == TRUE)
+		exit(0);
 }/*end of Fc_System_shutdown*/
 
 void Fc_Disconnect_Charger(void)
 {
 	/**Disconnect charger code**/
 	/*check the data transmission is done**/
+	if(Data_transmission_global == TRUE)
+		exit(0);
 }/*end of Fc_Disconnect_Charger*/
 
 void Fc_System_Sleep(void)
 {
 	/**System Sleep code**/
 	/*check the data transmission is done**/
+	if(Data_transmission_global == TRUE)
+		exit(0);
 }/*end of Fc_System_Sleep*/
 
 void Fc_System_Standby(void)
 {
 	/**System Standby Code**/
 	/*check the data transmission is done**/
+	if(Data_transmission_global == TRUE)
+		exit(0);
 }/*end of Fc_System_Standby*/
