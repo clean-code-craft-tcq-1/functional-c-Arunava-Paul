d1bff26fcaf6bc42dc80068addac6c1562da8eb4
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-03-02 16:18:34 +0530

    Update BMS_Hazard_Control.h

--- BMS_Hazard_Control.h
+++ BMS_Hazard_Control.h
@@ -14,5 +14,40 @@ extern void Fc_Disconnect_Charger(void);
 extern void Fc_System_Sleep(void);
 extern void Fc_System_Standby(void);
 
+#ifndef NULL_PTR
+
+#define NULL_PTR ((void *)0)
+#endif
+
+struct Hazard_Control_st
+{
+	Sysfault_type_en Sys_fault_index;
+	void *hazard_ctrl_fcpt;
+};
+
+#define HAZARD_CONTROL_ACCUMULATED_DATA			\
+{												\
+	{											\
+		Sysfault_none,						\
+		NULL_PTR,						\
+	},											\
+	{											\
+		SysFault_Standby,						\
+		&Fc_System_Standby,						\
+	},											\
+	{											\
+		Sysfault_Shutdown,						\
+		&Fc_System_shutdown,					\
+	},											\
+	{											\
+		Sysfault_Sleep,							\
+		&Fc_System_Sleep,						\
+	},											\
+	{											\
+		Sysfault_disconnect_charger,			\
+		&Fc_Disconnect_Charger					\
+	}											\
+}												
+
 
 #endif /* BMS_HARZARD_PROTECTION_H_ */
