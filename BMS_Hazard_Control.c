2a879d0632ec4705ae44e0bb65ed3c5ffa3a3a9c
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-03-02 16:18:07 +0530

    Update BMS_Hazard_Control.c
    Trying to reduce CYC

--- BMS_Hazard_Control.c
+++ BMS_Hazard_Control.c
@@ -5,65 +5,43 @@
  *      Author: PUL1KOR
  */
 #include "BMS_Common_Include.h"
-#include "BMS_Hazard_Control.h"
 #include "BMS_System_Check.h"
+#include "BMS_Hazard_Control.h"
 
 extern Sysfault_type_en Sysfault_global;
 extern int Data_transmission_global;
 
+Hazard_Control_st Hazard_Control[Sysfault_max_condition] = HAZARD_CONTROL_ACCUMULATED_DATA;
+
 void Fc_Hazard_Control(void)
 {
-	switch(Sysfault_global)
-	{
-	case Sysfault_none:
-		/*System cool*/
-		break;
-	case SysFault_Standby:
-		Fc_System_Standby();
-		break;
-	case Sysfault_Shutdown:
-		Fc_System_shutdown();
-		break;
-	case Sysfault_Sleep:
-		Fc_System_Sleep();
-		break;
-	case Sysfault_disconnect_charger:
-		Fc_Disconnect_Charger();
-		break;
-	default:
-		Fc_System_shutdown();
-		break;
-	}/*end of switch case*/
+	*(Hazard_Control[Sysfault_global].hazard_ctrl_fcpt)();
 }/*end of Fc_Hazard_Control*/
 
 void Fc_System_shutdown(void)
 {
 	/***Shutdown Code**/
 	/*check the data transmission is done**/
-	if(Data_transmission_global == TRUE)
-		exit(0);
-}/*end of Fc_System_shutdown*/
+}
 
 void Fc_Disconnect_Charger(void)
 {
 	/**Disconnect charger code**/
 	/*check the data transmission is done**/
-	if(Data_transmission_global == TRUE)
-		exit(0);
-}/*end of Fc_Disconnect_Charger*/
+}
 
 void Fc_System_Sleep(void)
 {
 	/**System Sleep code**/
 	/*check the data transmission is done**/
-	if(Data_transmission_global == TRUE)
-		exit(0);
-}/*end of Fc_System_Sleep*/
+}
 
 void Fc_System_Standby(void)
 {
 	/**System Standby Code**/
 	/*check the data transmission is done**/
 	if(Data_transmission_global == TRUE)
+	{
 		exit(0);
-}/*end of Fc_System_Standby*/
+	}
+}
